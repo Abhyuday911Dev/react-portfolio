31ad6351819d595d8a03bc5c4a79850d42ad4e15
Abhyuday <abhyuday911@gmail.com>
2023-06-18 01:55:52 +0530

    basic car added

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,4 +1,12 @@
-import { Bodies, Composite, Engine, Render, Runner } from "matter-js";
+import {
+  Bodies,
+  Composite,
+  Constraint,
+  Engine,
+  Render,
+  Runner,
+} from "matter-js";
+import Body from "matter-js/src/body/Body";
 import React, { useEffect, useRef } from "react";
 
 const Homepage = () => {
@@ -7,6 +15,76 @@ const Homepage = () => {
   const renderRef = useRef(null);
 
   useEffect(() => {
+    let w = window.innerWidth;
+    let h = window.innerHeight;
+
+    let car = function (xx, yy, width, height, wheelSize) {
+      var group = Body.nextGroup(true),
+        wheelBase = 20,
+        wheelAOffset = -width * 0.5 + wheelBase,
+        wheelBOffset = width * 0.5 - wheelBase,
+        wheelYOffset = 0;
+
+      var car = Composite.create({ label: "Car" }),
+        body = Bodies.rectangle(xx, yy, width, height, {
+          collisionFilter: {
+            group: group,
+          },
+          chamfer: {
+            radius: height * 0.5,
+          },
+          density: 0.0002,
+        });
+
+      var wheelA = Bodies.circle(
+        xx + wheelAOffset,
+        yy + wheelYOffset,
+        wheelSize,
+        {
+          collisionFilter: {
+            group: group,
+          },
+          friction: 0.8,
+        }
+      );
+
+      var wheelB = Bodies.circle(
+        xx + wheelBOffset,
+        yy + wheelYOffset,
+        wheelSize,
+        {
+          collisionFilter: {
+            group: group,
+          },
+          friction: 0.8,
+        }
+      );
+
+      var axelA = Constraint.create({
+        bodyB: body,
+        pointB: { x: wheelAOffset, y: wheelYOffset },
+        bodyA: wheelA,
+        stiffness: 1,
+        length: 0,
+      });
+
+      var axelB = Constraint.create({
+        bodyB: body,
+        pointB: { x: wheelBOffset, y: wheelYOffset },
+        bodyA: wheelB,
+        stiffness: 1,
+        length: 0,
+      });
+
+      Composite.addBody(car, body);
+      Composite.addBody(car, wheelA);
+      Composite.addBody(car, wheelB);
+      Composite.addConstraint(car, axelA);
+      Composite.addConstraint(car, axelB);
+
+      return car;
+    };
+
     // Create a Matter.js engine and renderer
     engineRef.current = Engine.create();
     renderRef.current = Render.create({
@@ -26,12 +104,22 @@ const Homepage = () => {
     // Add objects to the world
     var boxA = Bodies.rectangle(400, 200, 80, 80);
     var boxB = Bodies.rectangle(450, 50, 80, 80);
-    var ground = Bodies.rectangle(400, 610, 810, 60, {
-      isStatic: true,
-    });
 
     // add all of the bodies to the world
-    Composite.add(engineRef.current.world, [boxA, boxB, ground]);
+    Composite.add(engineRef.current.world, [boxA, boxB]);
+    Composite.add(engineRef.current.world, [
+      // walls
+      Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
+      Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
+      Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
+      Bodies.rectangle(0, 300, 50, 600, { isStatic: true }),
+    ]);
+
+    var scale = 0.9;
+    Composite.add(
+      engineRef.current.world,
+      car(150, 100, 150 * scale, 30 * scale, 30 * scale)
+    );
 
     // run the renderer
     Render.run(renderRef.current);
